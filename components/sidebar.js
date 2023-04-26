7708ebc5d0464a516a3640916b08e32db70b2dec
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-26 18:52:23 +0530

    1 file modified

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,12 +4,18 @@ import Navbar from './navbar';
 export default function Sidebar() {
   const [categories, setCategories] = useState([]);
   const fetchCategories = async () => {
-    const response = await fetch('api/category');
+    const response = await fetch('/api/categories');
     setCategories(response.catgory);
   };
   useEffect(() => {
     fetchCategories();
   }, []);
-  console.log(categories);
-  return <div></div>;
+  return (
+    <div>
+      {categories &&
+        categories.map((item) => {
+          return <div>{item}</div>;
+        })}
+    </div>
+  );
 }
