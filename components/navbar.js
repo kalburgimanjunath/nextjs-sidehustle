974c1d57a0e770db897a3638801d2f2a1f860a63
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-26 15:32:02 +0530

    2 files modified, 8 added

---
+++ components/navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-top">
+      <div>Sidehustle</div>
+      <ul>
+        <li>Home</li>
+        <li>Popular Ideas</li>
+        <li>Contribute your Ideas</li>
+      </ul>
+    </div>
+  );
+}
